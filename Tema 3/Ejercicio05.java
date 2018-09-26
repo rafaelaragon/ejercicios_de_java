3fa917b8e317cb1cf6968453b90ff76fa9afe502
Rafael Aragón Rodríguez <43571250+rafaelaragon@users.noreply.github.com>
2018-09-26 10:52:08 +0200

    Add files via upload

---
+++ Tema 3/Ejercicio05.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Crea un programa que calcule el área de un rectángulo.
+ * 
+ * @author Rafael Aragón Rodríguez
+ */
+public class Ejercicio05 {
+  public static void main(String[] args) {
+    System.out.println("Este programa calcula el área de un rectángulo.");
+    System.out.print("Introduce la base del rectángulo 'm,cm,km...': ");
+     String linea; 
+    linea = System.console().readLine();
+    int base = Integer.parseInt(linea);
+    System.out.print("Introduce la altura del rectángulo 'm,cm,km...': ");
+    linea = System.console().readLine();
+    int altura = Integer.parseInt(linea);
+    System.out.println("La base es " + base + " y la altura es " + altura + ".");
+    int prod = base * altura;
+    double div = (double)prod / 2;
+    System.out.println("El área del rectángulo es " + div + ".");
+  }
+}
