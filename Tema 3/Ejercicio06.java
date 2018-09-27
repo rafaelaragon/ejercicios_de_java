e5b44595acc91b288a6e3dffd42cb4c4ea15463b
Rafael Aragón Rodríguez <43571250+rafaelaragon@users.noreply.github.com>
2018-09-27 08:55:23 +0200

    Add files via upload

--- Tema 3/Ejercicio06.java
+++ Tema 3/Ejercicio06.java
@@ -10,13 +10,13 @@ public class Ejercicio06 {
     System.out.print("Introduce la base del triángulo 'm,cm,km...': ");
      String linea; 
     linea = System.console().readLine();
-    int base = Integer.parseInt(linea);
+    double base = Double.parseDouble(linea);
     System.out.print("Introduce la altura del triángulo 'm,cm,km...': ");
     linea = System.console().readLine();
-    int altura = Integer.parseInt(linea);
+    double altura = Double.parseDouble(linea);
     System.out.println("La base es " + base + " y la altura es " + altura + ".");
-    int prod = base * altura;
-    double div = (double)prod / 2;
-    System.out.println("El área del triángulo es " + div + ".");
+    double prod = base * altura;
+    double div = prod / 2;
+    System.out.printf("El área del triángulo es %.2f.\n", div);
   }
 }
