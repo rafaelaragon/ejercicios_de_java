af69f4d522eb136e50b49cbc09010ad465e04f92
Rafael Aragón Rodríguez <43571250+rafaelaragon@users.noreply.github.com>
2018-10-02 13:36:50 +0200

    Update Ejercicio06.java

--- Tema 2/Ejercicio06.java
+++ Tema 2/Ejercicio06.java
@@ -8,9 +8,9 @@ public class Ejercicio06 {
   public static void main(String[] args) {
     double eur = 49.95;
     double iva = eur * 0.21;
-    double tot = eur + iva;
+    double total = eur + iva;
     System.out.println("Precio sin IVA: " + eur + " euros.");
-    System.out.println("IVA: " + iva + " euros.");
-    System.out.println("Total a pagar: " + tot + " euros.");
+    System.out.printf("IVA: %.2f euros.\n", iva);
+    System.out.printf("Total a pagar: %.2f euros.\n", total);
   }
 }
