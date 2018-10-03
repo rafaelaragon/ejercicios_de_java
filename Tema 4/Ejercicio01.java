1b5c27b0343c91ddf7f0f12dc134374b432a7032
Rafael Aragón Rodríguez <43571250+rafaelaragon@users.noreply.github.com>
2018-10-03 08:40:31 +0200

    Update Ejercicio01.java

--- Tema 4/Ejercicio01.java
+++ Tema 4/Ejercicio01.java
@@ -10,38 +10,29 @@ public class Ejercicio01 {
     System.out.print("Este programa te dice qué asignatura ");
     System.out.println("toca a primera según el día que indiques.");
     System.out.print("Dime qué día quieres saber: ");
-    String dia = System.console().readLine();
+    String dia = System.console().readLine().toLowerCase();
     switch(dia) {
       case "lunes":
-      case "Lunes":
         System.out.println("Los lunes a primera hay Entornos de Desarrollo.");
         break;
       case "martes":
-      case "Martes":
         System.out.println("Los martes a primera hay Sistemas Informáticos.");
         break;
       case "miercoles":
       case "miércoles":
-      case "Miercoles":
-      case "Miércoles":
         System.out.println("Los miércoles a primera hay Programación.");
         break;
       case "jueves":
-      case "Jueves":
         System.out.println("Los jueves a primera hay Programación.");
         break;
       case "viernes":
-      case "Viernes":
         System.out.println("Los viernes a primera hay Sistemas Informáticos.");
         break;
       case "sabado":
       case "sábado":
-      case "Sabado":
-      case "Sábado":
         System.out.println("Los sábados a primera estás durmiendo.");
         break;
       case "domingo":
-      case "Domingo":
         System.out.println("Los domingos a primera estás durmiendo.");
         break;
       default:
