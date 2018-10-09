4d052db41ec940910a51940a7ed6ee6de90b9065
Rafael Aragón Rodríguez <43571250+rafaelaragon@users.noreply.github.com>
2018-10-09 14:12:14 +0200

    Add files via upload

---
+++ Tema 5/Ejercicio04.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Crea un programa que muestre los números del 320 al 160 hacia atrás de 5 en 5.
+ * Usa for.
+ * 
+ * @author Rafael Aragón Rodríguez
+ */
+public class Ejercicio04 {
+  public static void main(String[] args) {
+    for (int i = 320; i >= 160; i-= 20) {
+      System.out.print(i + " ");
+    }
+  }
+}
+
