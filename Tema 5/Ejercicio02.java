e4c3004f24b858dd48dc6a54333495aa940cf0e1
Rafael Aragón Rodríguez <43571250+rafaelaragon@users.noreply.github.com>
2018-10-09 14:05:25 +0200

    Add files via upload

---
+++ Tema 5/Ejercicio02.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Crea un programa que muestre múltiplos de 5 del 0 al 100 usando while.
+ * 
+ * @author Rafael Aragón Rodríguez
+ */
+public class Ejercicio02 {
+  public static void main(String[] args) {
+    int i = 0;
+    while (i < 100) {
+      i += 5;
+      System.out.print(i + " ");
+    }
+  }
+}
