2b35d876aa959a6f81ec43cdd2de9e98f29b0b21
Rafael Aragón Rodríguez <43571250+rafaelaragon@users.noreply.github.com>
2018-10-09 14:02:42 +0200

    Add files via upload

---
+++ Tema 5/Ejercicio01.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Crea un programa que muestre múltiplos de 5 del 0 al 100 usando for.
+ * 
+ * @author Rafael Aragón Rodríguez
+ */
+public class Ejercicio01 {
+  public static void main(String[] args) {
+    for (int i = 5; i <= 100; i += 5) {
+      System.out.print(i + " ");
+    }
+  }
+}
