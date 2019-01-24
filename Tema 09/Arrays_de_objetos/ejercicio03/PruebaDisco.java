9d811f5f64805d1854857618f70d65309433bddc
Rafael Aragón Rodríguez <43571250+rafaelaragon@users.noreply.github.com>
2019-01-24 08:32:31 +0100

    Arreglado un error en el bucle final

--- Tema 09/Arrays_de_objetos/ejercicio03/PruebaDisco.java
+++ Tema 09/Arrays_de_objetos/ejercicio03/PruebaDisco.java
@@ -19,7 +19,7 @@ public class PruebaDisco {
     d[2] = new Disco("Aproximaciones", "Fernando Lamadrid", "Jazz-Funk", "10");
     d[3] = new Disco("Mesmerize", "System of a Down", "Hard Rock  ", "11");
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
       System.out.println("Disco nº" + (i + 1) + "\n");
       System.out.println("Título: " + d[i].getTitulo());
       System.out.println("Artista: " + d[i].getArtista());
